1926345c42eb808ca029659d3fd2b98605ea9e34
will <willjoemolloy@gmail.com>
2016-10-02 14:35:22 +1300

    main menu completed - will add css later

--- src/Voxspell/Main.java
+++ src/Voxspell/Main.java
@@ -1,41 +1,53 @@
 package Voxspell;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
+/**
+ * Launches the application, also controller for the Main Menu.
+ *
+ * @author Will Molloy
+ */
 public class Main extends Application {
 
-    private Stage window;
+    private static Stage window;
+    private static Parent mainMenu, spellingQuiz;//, viewStats, settings;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws IOException {
         window = primaryStage;
 
-        Parent mainMenu = FXMLLoader.load(getClass().getResource("Main_Menu.fxml"));
-
+        mainMenu = FXMLLoader.load(getClass().getResource("Main_Menu.fxml"));
+        spellingQuiz = FXMLLoader.load(getClass().getResource("Spelling_Quiz.fxml"));
 
         window.setTitle("VoxSpell");
-        window.setScene(new Scene(mainMenu, 640, 480));
+        window.setScene(new Scene(mainMenu));
         window.show();
     }
-/*
-    private void makeButtons() {
-        spellingQuizBtn = new Button("Spelling Quiz");
-        spellingQuizBtn.setOnAction(e -> window.setScene(spellingQuiz));
-
-        viewStatsBtn = new Button("View Statistics");
-        viewStatsBtn.setOnAction(e -> window.setScene(viewHistoryStats));
-
-        returnToMainMenuBtn = new Button("Return to the Main Menu");
-        returnToMainMenuBtn.setOnAction(e -> window.setScene(mainMenu));
-    }
-*/
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+
+    @FXML
+    public void handleQuizButton(ActionEvent actionEvent) {
+        window.setScene(new Scene(spellingQuiz));
+    }
+
+    @FXML
+    public void handleStatisticsButton(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleSettingsButton(ActionEvent actionEvent) {
     }
 }
--- src/Voxspell/SpellingQuiz.java
+++ src/Voxspell/SpellingQuiz.java
@@ -1,7 +1,14 @@
 package Voxspell;
 
+import javafx.event.ActionEvent;
+
 /**
  * Created by will on 2/10/16.
  */
 public class SpellingQuiz {
+    public void handleQuizButton(ActionEvent actionEvent) {
+    }
+
+    public void handleSettingsButton(ActionEvent actionEvent) {
+    }
 }
